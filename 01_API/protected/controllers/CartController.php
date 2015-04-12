080957e44aa0524cf8d9717af2b9ec20e3566458
hieunc <hieunc18@gmail.com>
2015-04-12 17:26:56 +0700

    API>Cart

--- 01_API/protected/controllers/CartController.php
+++ 01_API/protected/controllers/CartController.php
@@ -8,6 +8,72 @@ class CartController extends Controller
         $this->forward("search");
     }
 
+    public function actionEdit(){
+        $cart = null;
+        $this->_post_data = Helpers::getJsonData();
+        $session = Yii::app()->session;
+
+        if($session->contains(self::SESSION_KEY)){
+            $cart = $session[self::SESSION_KEY];
+            $session->remove(self::SESSION_KEY);
+        }
+
+        if($cart == null){
+            Helpers::_sendResponse(200, json_encode(array(
+                'error' => array(
+                    "error_code" => "1006",
+                    "error_message" => "No Items found.",
+                ))));
+        }
+
+        if(!isset($this->_post_data['cartItems'])){
+            Helpers::_sendResponse(200, json_encode(array(
+                'error' => array(
+                    "error_code" => "1004",
+                    "error_message" => "No Item to add.",
+                ))));
+        }
+
+        $_result = array();
+
+        foreach($this->_post_data['cartItems'] as $item){
+            if($cart !=null){
+                foreach($cart as $existsItem){
+                    if($item['id'] == $existsItem['id']){
+                        $existsItem['qty'] = $item['qty'];
+                    }
+                    $_result[] = $existsItem;
+                }
+            }
+        }
+        $cart = $_result;
+        $session->add(self::SESSION_KEY, $cart);
+
+        Helpers::_sendResponse(200, json_encode(array(
+            'status' => array(
+                "status_code" => "1007",
+                "status_message" => "Item is edited successly.",
+            ))));
+    }
+
+    public function actionDelete(){
+
+    }
+
+    public function actionDestroy(){
+        $session = Yii::app()->session;
+
+        if($session->contains(self::SESSION_KEY)){
+            $session->remove(self::SESSION_KEY);
+        }
+
+        Helpers::_sendResponse(200, json_encode(array(
+            'status' => array(
+                "status_code" => "1008",
+                "status_message" => "Cart has been destroied.",
+            ))));
+    }
+
     public function actionAdd(){
         $cart = null;
         $this->_post_data = Helpers::getJsonData();
@@ -26,10 +92,27 @@ class CartController extends Controller
             ))));
         }
 
+        $_result= array();
+
+        $isNewCartItem = true;
         foreach($this->_post_data['cartItems'] as $item){
-            $cart[] = $item;
+            if($cart != null)
+            {
+                foreach($cart as $existsItem){
+                    if($item['id'] == $existsItem['id']){
+                        $existsItem['qty'] += $item['qty'];
+                        $_result[] = $existsItem;
+                        $isNewCartItem = false;
+                    }
+                }
+            }
+            if($isNewCartItem){
+                $_result[] = $item;
+            }
+            $isNewCartItem = true;
         }
 
+        $cart = $_result;
 
         $session->add(self::SESSION_KEY, $cart);
 
@@ -68,4 +151,6 @@ class CartController extends Controller
             ))));
         }
     }
+
+
 }
