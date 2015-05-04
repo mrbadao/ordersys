b5a54a2ac725a690eec160bc0fc247a1a7709dbc
hieunc <hieunc18@gmail.com>
2015-05-04 21:34:47 +0700

    API> Delivery> orderdetail

--- 01_API/protected/controllers/DeliveryController.php
+++ 01_API/protected/controllers/DeliveryController.php
@@ -73,6 +73,70 @@ class DeliveryController extends Controller{
             ))));
     }
 
+    public function actionGetDeliveryOrderDetail(){
+        $this->_post_data = Helpers::getJsonData();
+
+        if(isset($this->_post_data['token']) && isset($this->_post_data['staff_id'])) {
+            $token = DeliveryToken::model()->findByAttributes(array('token' => $this->_post_data['token'], 'staff_id' => $this->_post_data['staff_id']));
+
+            if ($token) {
+                $_result = array();
+                $_result['count'] = 0;
+                $_result['orders'] = array();
+
+                $id = isset($this->_post_data['order_id']) ? $this->_post_data['order_id'] : null;
+
+                if ($id) {
+                    $order = ContentOrder::model()->findByPk($id);
+
+                    if ($order) {
+                        $_result = array();
+                        $_result['count'] = 0;
+                        $_result['order'] = array();
+
+                        $c = new CDbCriteria();
+                        $c->addCondition(' order_id = ' . $id, 'AND');
+                        $c->order = 'id DESC';
+
+                        $_result['count'] = OrderRelation::model()->count($c);
+
+                        if ($_result['count'] > 0) {
+                            $orderDetail = OrderRelation::model()->findAll($c);
+
+                            foreach ($orderDetail as $item) {
+                                $product = Helpers::getProduct($item->product_id);
+                                if ($product) {
+                                    $_OrderItem['id'] = $product->id;
+                                    $_OrderItem['name'] = $product->name;
+                                    $_OrderItem['price'] = $item->price;
+                                    $_OrderItem['qty'] = $item->qty;
+                                    $_result['order'][] = $_OrderItem;
+                                }
+                            }
+
+                            Helpers::_sendResponse(200, json_encode($_result));
+                        }
+                    }
+                }
+                Helpers::_sendResponse(200, json_encode(array(
+                    'error' => array(
+                        "error_code" => "1011",
+                        "error_message" => "Order not found.",
+                    ))));
+            }
+            Helpers::_sendResponse(200, json_encode(array(
+                'error' => array(
+                    "error_code" => "1015",
+                    "error_message" => "Invalid Token.",
+                ))));
+        }
+        Helpers::_sendResponse(200, json_encode(array(
+            'error' => array(
+                "error_code" => "1015",
+                "error_message" => "Invalid Token.",
+            ))));
+    }
+
     public function actionGetDeliveryOrders(){
         $this->_post_data = Helpers::getJsonData();
 
