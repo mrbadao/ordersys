fd7b48476c9f772ed89659fd9056386baa8b8383
hieunc <hieunc@aoi-sys.vn>
2015-05-12 08:40:51 +0700

    01_API->Delivery->login

--- 02_Front/protected/modules/category/controllers/DefaultController.php
+++ 02_Front/protected/modules/category/controllers/DefaultController.php
@@ -3,13 +3,36 @@
 class DefaultController extends Controller
 {
 	const SESS_KEY = '_CATEGORIES';
+	const LIMIT = 10;
 
 	public function actionIndex()
     {
+        $cat_name = null;
+        $this->layout = "main";
+
         $id = isset($_GET['id']) ? $_GET['id'] : null;
         $page = isset($_GET['page']) ? $_GET['page'] : 1;
 
-        var_dump($id);
-        die;
+        $cat_name = ContentCategories::model()->findByPk($id);
+
+        if($cat_name == null) throw new CHttpException(404,'Url Site not exits');
+
+        $c = new CDbCriteria();
+        $c->order = "id DESC";
+        $c->addCondition('del_flg = 0', 'AND');
+        $c->addCondition('category_id = '.$id, 'AND');
+
+        $count = ContentProduct::model()->count($c);
+
+        $c->limit = self::LIMIT;
+        $c->offset = ($page - 1) * self::LIMIT;
+
+        $items = ContentProduct::model()->findAll($c);
+
+        $pages = new CPagination($count);
+        $pages->pageSize = $c->limit;
+        $pages->applyLimit($c);
+
+        $this->render('index', compact('items', 'pages', 'cat_name'));
     }
 }
