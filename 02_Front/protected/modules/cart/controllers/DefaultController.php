9b95c59effeb252139188b91f8b638972d600eb9
hieunc <hieunc@aoi-sys.vn>
2015-05-12 17:40:49 +0700

    01_API->Delivery->login

--- 02_Front/protected/modules/cart/controllers/DefaultController.php
+++ 02_Front/protected/modules/cart/controllers/DefaultController.php
@@ -2,6 +2,8 @@
 
 class DefaultController extends Controller
 {
+    const SESSION_KEY = '_CART';
+
     public function actionIndex()
     {
         throw new CHttpException(404,'Page not exists.');
@@ -14,6 +16,34 @@ class DefaultController extends Controller
         $id = isset($_POST['pid']) ? $_POST['pid'] : null;
         $qty = isset($_POST['qty']) ? $_POST['qty'] : null;
 
-        echo $id.$qty;
+        if($id != null && $qty != null){
+            $session = Yii::app()->session;
+
+            if($session->contains(self::SESSION_KEY)){
+                $cart = $session[self::SESSION_KEY];
+                $session->remove(self::SESSION_KEY);
+            }
+
+            $_result = array();
+
+
+                if($cart !=null){
+                    foreach($cart as $existsItem){
+                        if($item['id'] == $existsItem['id']){
+                            $existsItem['qty'] = $item['qty'];
+                        }
+                        $_result[] = $existsItem;
+                    }
+                }
+
+            $cart = $_result;
+            $session->add(self::SESSION_KEY, $cart);
+
+            Helpers::_sendResponse(200, json_encode(array(
+                'status' => array(
+                    "status_code" => "1007",
+                    "status_message" => "Item is edited successly.",
+                ))));
+        }
     }
 }
