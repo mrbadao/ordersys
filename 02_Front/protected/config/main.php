d771bfa4938fef7edfd929ccecba989a84cfc7c7
hieunc <hieunc@aoi-sys.vn>
2015-05-12 16:36:37 +0700

    01_API->Delivery->login

--- 02_Front/protected/config/main.php
+++ 02_Front/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Owl Food Shop',
+	'name'=>'Thức ăn cú đêm',
 	'language' => 'en',
     'defaultController' =>'Content',
 
