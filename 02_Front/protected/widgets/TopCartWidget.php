4a8b70c30f824eb5b74e72900a4b5bb8d8abbea9
hieunc <hieunc@aoi-sys.vn>
2015-05-13 09:01:12 +0700

    01_API->Delivery->login

--- 02_Front/protected/widgets/TopCartWidget.php
+++ 02_Front/protected/widgets/TopCartWidget.php
@@ -1,17 +1,42 @@
 <?php
 
-class TopCartWidget extends CLinkPager {
+class TopCartWidget extends CLinkPager
+{
+    public $session;
 
-    public function init() {
-
+    public function init()
+    {
+        $this->session = Yii::app()->session;
     }
 
     /**
      * Executes the widget.
      * This overrides the parent implementation by displaying the generated page buttons.
      */
-    public function run() {
-        $this->render('top-cart-widget');
+    public function run()
+    {
+        $count = 0;
+        $total = 0;
+        $cart = array();
+
+        if ($this->session->contains(self::SESSION_KEY)) {
+            $cart = $this->session[self::SESSION_KEY];
+        }
+
+        $count = count($cart);
+
+        if ($count > 0) {
+            for ($i = 0; $i < count($cart); $i++) {
+                $product = Helpers::getProduct($cart[$i]['id']);
+
+                if ($product) {
+                    $total += $product->price * $cart[$i]['qty'];
+                }
+            }
+            $total = number_format($total);
+        }
+
+        $this->render('top-cart-widget', compact('count', 'total'));
     }
 
 }
