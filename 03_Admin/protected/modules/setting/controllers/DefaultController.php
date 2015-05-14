deb1050899ade8001b5811c8a5114bf0088705d6
hieunc <hieunc18@gmail.com>
2015-05-14 07:23:04 +0700

    API> Delivery> orderdetail

--- 03_Admin/protected/modules/setting/controllers/DefaultController.php
+++ 03_Admin/protected/modules/setting/controllers/DefaultController.php
@@ -2,34 +2,38 @@
 
 class DefaultController extends Controller
 {
-	public function actionIndex()
-	{
-		$this->title='Setting | CMS Order Sys';
-		$msg = false;
-		$siteSettings = null;
-		$settingKey = array('Email', 'Facebook', 'Twitter', 'Gplus', 'Phone', 'Mobile');
+    public function actionIndex()
+    {
+        $this->title = 'Setting | CMS Order Sys';
+        $msg = false;
+        $siteSettings = null;
+        $settingKey = array('Email', 'Facebook', 'Twitter', 'Gplus', 'Phone', 'Mobile');
 
-		foreach($settingKey as $item){
-			if(isset($_POST['setting'])) {
-				$siteSettings[] = self::getSetting($item, $_POST['setting'][$item]);
-				$msg = true;
-			}
-			$siteSettings[] = self::getSetting($item, '');
-		}
+        foreach ($settingKey as $item) {
+            if (isset($_POST['setting'])) {
+                $siteSettings[] = self::getSetting($item, $_POST['setting'][$item]);
+                $msg = true;
+            } else {
+                $siteSettings[] = self::getSetting($item, '');
+            }
+        }
 
-		$this->render('index', compact('siteSettings', 'msg'));
-	}
+        $this->render('index', compact('siteSettings', 'msg'));
+    }
 
-	private function getSetting($key, $val){
-		$setting = Setting::model()->findByAttributes(array('key' => $key));
+    private function getSetting($key, $val)
+    {
+        $setting = null;
+        $setting = Setting::model()->findByAttributes(array('key' => $key));
 
-		if($setting) return $setting;
-		else{
-			$setting = new Setting();
-			$setting->key = $key;
-			$setting->value = $val;
-			$setting->save(false);
-			return $setting;
-		}
-	}
+        if ($setting == null) {
+            $setting = new Setting();
+            $setting->key = $key;
+        }
+
+        $setting->value = $val;
+        $setting->save(false);
+
+        return $setting;
+    }
 }
