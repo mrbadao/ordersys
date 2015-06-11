15bd1cd89d01c6f6528df802ec3766f1cb1f090e
hieunc <hieunc@aoi-sys.vn>
2015-06-11 18:05:50 +0700

    03_Admin > Modules > Feedback

--- 03_Admin/protected/modules/saleoff/controllers/DefaultController.php
+++ 03_Admin/protected/modules/saleoff/controllers/DefaultController.php
@@ -14,4 +14,20 @@ class DefaultController extends Controller
 
         return $this->render('edit', compact('ContentSaleoff','contentCats'));
     }
+
+    public function actionGetCats(){
+        if(Yii::app()->request->isAjaxRequest){
+            $id=$_POST['id'];
+
+            if($id!=null){
+                $products= ContentProduct::model()->findAllByAttributes(array('category_id' => $id));
+                $result = array();
+                foreach($products as $product){
+                    $result[] = $product->attributes;
+                }
+
+                echo json_encode($result);
+            }
+        }
+    }
 }
