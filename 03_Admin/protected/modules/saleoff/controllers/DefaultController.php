cd2bc13e4c1f88e9ca3e599c07da283318abb6a1
hieunc <hieunc@aoi-sys.vn>
2015-06-12 12:41:52 +0700

    03_Admin > Modules > Feedback

--- 03_Admin/protected/modules/saleoff/controllers/DefaultController.php
+++ 03_Admin/protected/modules/saleoff/controllers/DefaultController.php
@@ -2,6 +2,8 @@
 
 class DefaultController extends Controller
 {
+    const SESS_KEY = '_SALEOFF';
+
 	public function actionIndex()
 	{
 		$this->render('index');
@@ -67,6 +69,79 @@ class DefaultController extends Controller
         $this->setTitle("CMS Order Sys | saleoff detail");
         return $this->render('view', compact('contendSaleoff','itemList', 'msg'));
 
+    }
+
+    public function actionSearch(){
+        $this->title='CMS Order Sys | Manage Saleoff';
+        $search['name'] = '';
+
+
+        $session = Yii::app()->session;
+
+        if(isset($_POST['search']))
+        {
+
+            if($session->contains(self::SESS_KEY))
+                $session->remove(self::SESS_KEY);
+
+            $data['search'] = $_POST['search'];
+            $data['page'] = 1;
+            $session->add(self::SESS_KEY, $data);
+        }
+
+
+        $c = new CDbCriteria();
+        $c->alias = "t";
+        $c->together = true;
+
+        if(isset($session[self::SESS_KEY]['search']))
+        {
+
+            $search = $session[self::SESS_KEY]['search'];
+            foreach($search as $k => $v)
+            {
+                if(!isset($v) || $v === '')
+                {
+                    continue;
+                }
+                switch($k)
+                {
+                    case 'name':
+                        $c->compare($k, $v, true,'AND');
+                        break;
+//                    case 'category_id':
+//                        $c->compare($k, $v, false,'AND');
+//                        break;
+//                    case 'del_flg':
+//                        if($v != '')
+//                            $c->compare($k, $v, true,'AND');
+//                        break;
+                }
+            }
+        }
+
+        $sess_data = $session[self::SESS_KEY];
+        if(isset($_GET['page']))
+            $page = $sess_data['page'] = $_GET['page'];
+
+        else
+            $page = $sess_data['page'] = 1;
+        $session->add(self::SESS_KEY,$sess_data);
+
+        $c->select = 't.*';
+        $c->group = 't.id';
+        $c->order = 't.id DESC';
+        $count = ContentSaleoff::model()->count($c);
+
+        $nodata = ($count)?false:true;
+        $c->limit = 10;
+        $c->offset = $c->limit * ($page-1);
+        $items = ContentSaleoff::model()->findAll($c);
+        $pages = new CPagination($count);
+        $pages->pageSize = $c->limit;
+        $pages->applyLimit($c);
+
+        $this->render('index',compact('items', 'catItems', 'count','pages','search','nodata'));
     }
 
     public function actionGetPros(){
